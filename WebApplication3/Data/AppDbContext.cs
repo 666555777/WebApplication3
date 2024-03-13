c107b0dbaa9ef3d5593ffa49e4bfbc047c5e85e7
Kushal721 <bhattakushal1@gmail.com>
2024-03-13 09:34:42 +0545

    Add project files.

---
+++ WebApplication3/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models.Student;
+
+namespace WebApplication3.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ WebApplication3/Models/Student/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models.Student
+{
+    public class Student
+    {
+        [Key]
+        public int LMUID { get; set; }
+        public int Name { get; set; }
+        public int Email { get; set; }
+        public int FypTitle { get; set; }
+    }
+}
